8f535c4a3fb09ad3fc11c597fdb248d9698dd4d9
garyodernichts <naj02@gmx.de>
2020-02-01 16:54:40 +0100

    Added support for creating services

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,17 @@
+import client.dcerpc.msvcctl.BetterServiceControlManagerService;
 import com.hierynomus.smbj.SMBClient;
 import com.hierynomus.smbj.auth.AuthenticationContext;
 import com.hierynomus.smbj.connection.Connection;
 import com.hierynomus.smbj.session.Session;
-import com.rapid7.client.dcerpc.mssrvs.ServerService;
-import com.rapid7.client.dcerpc.mssrvs.dto.*;
-import com.rapid7.client.dcerpc.msvcctl.ServiceControlManagerService;
 import com.rapid7.client.dcerpc.msvcctl.dto.ServiceHandle;
 import com.rapid7.client.dcerpc.msvcctl.dto.ServiceManagerHandle;
+import com.rapid7.client.dcerpc.msvcctl.dto.enums.ServiceError;
+import com.rapid7.client.dcerpc.msvcctl.dto.enums.ServiceStartType;
+import com.rapid7.client.dcerpc.msvcctl.dto.enums.ServiceType;
 import com.rapid7.client.dcerpc.transport.RPCTransport;
 import com.rapid7.client.dcerpc.transport.SMBTransportFactories;
 
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
 
@@ -22,13 +22,26 @@ public class Main {
             Session session = smbConnection.authenticate(smbAuthenticationContext);
 
             RPCTransport t = SMBTransportFactories.SVCCTL.getTransport(session);
-            ServiceControlManagerService s = new ServiceControlManagerService(t);
+            BetterServiceControlManagerService s = new BetterServiceControlManagerService(t);
             ServiceManagerHandle handle = s.openServiceManagerHandle();
-            System.out.println(handle);
+
+            ServiceHandle testservice = s.createService(handle,
+                                        "testtest",
+                                        "testtest",
+                                        ServiceType.WIN32_OWN_PROCESS,
+                                        ServiceStartType.DEMAND_START,
+                                        ServiceError.NORMAL,
+                                        "C:/test.exe",
+                                        null,
+                                        0,
+                                        null,
+                                        null,
+                                        null);
+
+            s.startService(testservice);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
---
+++ src/main/java/client/dcerpc/msvcctl/BetterServiceControlManagerService.java
@@ -0,0 +1,47 @@
+package client.dcerpc.msvcctl;
+
+import client.dcerpc.msvcctl.messages.RCreateServiceWRequest;
+import com.rapid7.client.dcerpc.msvcctl.ServiceControlManagerService;
+import com.rapid7.client.dcerpc.msvcctl.dto.ServiceHandle;
+import com.rapid7.client.dcerpc.msvcctl.dto.ServiceManagerHandle;
+import com.rapid7.client.dcerpc.msvcctl.dto.enums.ServiceError;
+import com.rapid7.client.dcerpc.msvcctl.dto.enums.ServiceStartType;
+import com.rapid7.client.dcerpc.msvcctl.dto.enums.ServiceType;
+import com.rapid7.client.dcerpc.transport.RPCTransport;
+
+import java.io.IOException;
+
+public class BetterServiceControlManagerService extends ServiceControlManagerService {
+    public BetterServiceControlManagerService(RPCTransport transport) {
+        super(transport);
+    }
+
+    public ServiceHandle createService(final ServiceManagerHandle serviceManagerHandle,
+                                       final String serviceName,
+                                       final String displayName,
+                                       final ServiceType serviceType,
+                                       final ServiceStartType serviceStartType,
+                                       final ServiceError errorControl,
+                                       final String binaryPathName,
+                                       final String loadOrderGroup,
+                                       final int tagId,
+                                       final String[] dependencies,
+                                       final String serviceStartName,
+                                       final String password) throws IOException {
+        final RCreateServiceWRequest request = new RCreateServiceWRequest(serviceManagerHandle.getBytes(),
+                parseWCharNT(serviceName),
+                parseWCharNT(displayName),
+                FULL_ACCESS,
+                serviceType.getValue(),
+                serviceStartType.getValue(),
+                errorControl.getValue(),
+                parseWCharNT(binaryPathName),
+                parseWCharNT(loadOrderGroup),
+                tagId,
+                dependencies,
+                parseWCharNT(serviceStartName),
+                password);
+        return new ServiceHandle(callExpectSuccess(request, "RCreateServiceW").getHandle());
+    }
+
+}
